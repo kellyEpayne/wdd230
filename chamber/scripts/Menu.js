ec769a0da91d92c495a30bf8ff758bd284e6ef6d
kellyEpayne <86086394+kellyEpayne@users.noreply.github.com>
2022-10-08 11:28:34 -0600

    added chamber folder

---
+++ chamber/scripts/Menu.js
@@ -0,0 +1,8 @@
+const menubutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.naviagation');
+
+menubutton.addEventListener('click', () => 
+{mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () => 
+{if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
---
+++ chamber/scripts/copyright.js
@@ -0,0 +1,9 @@
+let oLastModif = new Date(document.lastModified);
+
+const date = new Date();
+let currentYear = date.getFullYear()
+
+let copyrightinfo = `©${currentYear} | Kelly Payne | WDD230| Last Updated: ${oLastModif}`
+
+
+document.querySelector('.copyright').textContent = copyrightinfo;
