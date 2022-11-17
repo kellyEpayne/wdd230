243ea80cd1d5695fdc9ff51e3c4cae176e20b198
kellyEpayne <86086394+kellyEpayne@users.noreply.github.com>
2022-11-16 17:50:42 -0700

    Home page updates

---
+++ chamber/scripts/homeShow.js
@@ -0,0 +1,56 @@
+
+function spotlight(business){
+
+    
+    let card = document.createElement('div');
+    let logo = document.createElement('img');
+    let name = document.createElement('h2');
+    let location = document.createElement('p');
+    let phoneNumber = document.createElement('p');
+    let url = document.createElement('a');
+    
+    logo.setAttribute('src', business.image);
+    logo.setAttribute('alt', business.name + " logo");
+    logo.setAttribute('loading', 'lazy');
+
+    name.textContent = business.name;
+    location.textContent = business.address;
+    phoneNumber.textContent = business.phoneNumber;
+    url.textContent = business.url;
+
+    url.setAttribute('href', business.url)
+
+    card.appendChild(name);
+    card.appendChild(logo);
+    card.appendChild(phoneNumber);
+    card.appendChild(url);
+    card.appendChild(location);
+
+    document.querySelector('.spotlight').appendChild(card);
+}
+
+function limit(business){
+    return business.level >= 3
+}
+
+function shuffle(array){
+    array.sort(() => Math.random() - 0.5);
+}
+
+fetch('./json/data.json')
+.then(function (response) {
+    return response.json();
+})
+.then(function (jsonObject) {
+    const businesses = jsonObject['businesses'];
+    hold = businesses.filter(limit);
+
+    if (hold.length > 3){
+        shuffle(hold);
+        spotlight(hold[0]);
+        spotlight(hold[1]);
+        spotlight(hold[2]);
+    } else {
+        hold.forEach(spotlight);
+    }
+});
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,16 +1,49 @@
-const tempature = 50
-const windSpeed = 5
+const apiURL = 
+    'https://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&APPID=83ce386cabbd9497aa668a6ee115617e';
 
-const htmlWindChill = document.getElementById("windchill")
-const htmlTemp = document.getElementById("tempature")
-const htmlWindSpeed = document.getElementById("windspeed")
+const htmlWindChill = document.getElementById("windchill");
+const htmlTemp = document.getElementById("tempature");
+const htmlWindSpeed = document.getElementById("windspeed");
+const weatherIcon = document.querySelector(".weatherIcon");
+const currentWeather = document.querySelector(".weatherType");
+const weatherLocation = document.querySelector(".weatherLocation");
 
-htmlTemp.textContent = tempature
-htmlWindSpeed.textContent = windSpeed
-
-if( tempature <= 50 & windSpeed > 3){
-    let windChillCal = 35.74 + 0.6215*tempature - 35.75*windSpeed**0.16 + 0.4275*tempature*windSpeed**0.16
-    windChillCal = Math.round(windChillCal * 10) / 10
-    htmlWindChill.textContent = windChillCal
+async function apiFetch(){
+    try{
+        const response = await fetch(apiURL);
+        if (response.ok){
+            const data = await response.json();
+            console.log(data);
+            displayWeather(data);
+        } else{
+            console.log('Response not OK');
+        }
+    } catch(error){
+        console.log(`Error: ${error.message}`);
+    }
 }
 
+function capitalize(string){
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+}
+
+function displayWeather(data){
+    const tempature = data.main.temp;
+    const windSpeed = data.wind.speed;
+
+    htmlTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
+    htmlWindSpeed.textContent = windSpeed;
+    let desc = capitalize(data.weather[0].description);
+    currentWeather.textContent = desc;
+    weatherLocation.textContent = `Location: Denver`;
+    weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    weatherIcon.setAttribute('alt', `Icon of the current weather in Denver which is ${desc}`);
+    
+    if( tempature <= 50 & windSpeed > 3){
+        let windChillCal = 35.74 + 0.6215*tempature - 35.75*windSpeed**0.16 + 0.4275*tempature*windSpeed**0.16;
+        windChillCal = Math.round(windChillCal * 10) / 10;
+        htmlWindChill.textContent = windChillCal;
+    }
+}
+
+apiFetch();
